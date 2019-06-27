539226b0a586d5084249c902825af3a1c5584629
詹展 <@alibaba.comna>
2019-06-27 20:36:33 +0800

    项目基本结构搭建完成

--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
+//1。 引入elemrntui
+import ElementUI from 'element-ui';
+
+//引入css文件
+import 'element-ui/lib/theme-chalk/index.css';
+
+// import "./assets/css/index.css";
+
+//2。安装elementui
+Vue.use(ElementUI)
+
 
 Vue.config.productionTip = false;
 
